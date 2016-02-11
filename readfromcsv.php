019bbf450b11b8380c87ab808ec3560fb14f3d2f
Ermolaev Aleksey (WORK) <ermolaev@spb.mmbank.ru>
2016-02-11 10:28:19 +0300

    Add function: read data from CSV file

---
+++ readfromcsv.php
@@ -0,0 +1,22 @@
+<?php
+$row = 1;
+$separator = ";";
+
+
+if (($handle = fopen("TestCSV.csv", "r")) !== FALSE) {
+
+    while (($data = fgetcsv($handle, 1000, $separator)) !== FALSE) {
+        $num = count($data);
+        print "<p> $num полей в строке $row: <br /></p>\n";
+        $row++;
+        for ($c=0; $c < $num; $c++) {
+            print $data[$c] . "<br />\n";
+        }
+    }
+
+    fclose($handle);
+
+}
+
+?>
+
